e3496391323a99e14eaad96ff8c6cf6b16f54f51
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-10-02 10:59:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
 import Navbar from './Navbar';
-/*import Home from './pages/Home';
-import Search from './pages/Reports';
-import Reports from './pages/Reports';*/
 
 function App() {
-	/*let Component;
-	switch(window.location) {
-		case "/":
-		Component = App;	
-		break;
-		
-		case "/search":
-			Component = Search;
-			break;
-
-		case "/reports":
-			Component = Reports;
-			break;
-
-		default:
-			break;
-	}*/
 	return <Navbar />
 }
 
