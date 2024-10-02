0a50bbd57661c0f9b7560b84628601d8946899f0
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-10-02 13:07:37 -0400

    new pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import Navbar from './Navbar';
+import Navbar from './Navbar'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Reports from './pages/Reports'
 
 function App() {
-	return <Navbar />
+	let Component;
+	switch(window.location.pathname) {
+		case "/":
+			Component = <Home />
+			break;
+
+		case "/search":
+			Component = <Search />
+			break;
+
+		case "/reports":
+			Component = <Reports />
+			break;
+
+		default:
+			break;
+	}
+	return (
+		<>
+			<Navbar />
+			<Component />
+		</>
+	)
 }
 
 export default App;
