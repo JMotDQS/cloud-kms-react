83e8c666b671971d58f293f0718df9641b1c8692
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-10-11 16:55:03 -0400

    added bacode reading and collection

--- src/pages/Labels.js
+++ src/pages/Labels.js
@@ -1,9 +1,36 @@
+import React, { useState, useRef, useEffect } from 'react';
+import BarcodeReader from 'react-barcode-reader';
+
 export default function Labels() {
+	const inputRef = useRef(null);
+	const [barcode, setBarcode] = useState('');
+
+	useEffect(() => {
+		if (inputRef.current) {
+			inputRef.current.focus();
+		}
+	}, []);
+
+	const handleScan = (data) => {
+		setBarcode(data);
+	};
+
+	const handleError = (err) => {
+		console.error(err);
+	};
+
 	return(
 		<>
 			<div className="grid-container grid-container-labels">
 				<div className="card card-labels">
-					Labels
+					<p>Labels</p>
+					<div>
+						{/*<form>
+							<input ref={inputRef} type="text" id="label-vin-input" readOnly value={barcode} />
+						</form>*/}
+						<BarcodeReader onError={handleError} onScan={handleScan} />
+						<p>{barcode}</p>
+					</div>
 				</div>
 			</div>
 		</>
