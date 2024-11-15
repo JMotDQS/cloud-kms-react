9f1105791da5c8930cd1b744e4d5e57185b78d20
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-11-15 15:18:09 -0500

    Removed the need for a separate 'link' item

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
 			section: "Labels",
 			body: "Print QR Code VIN labels",
 			icon: "fas fa-qrcode",
-			link: "/labels",
+			link: "labels",
 			class: "card-labels"
 		},
 		{
@@ -22,7 +22,7 @@ export default function Home() {
 			section: "Check In",
 			body: "Key Check In",
 			icon: "fas fa-sign-in-alt",
-			link: "/checkin",
+			link: "checkin",
 			class: "card-checkin"
 		},
 		{
@@ -30,7 +30,7 @@ export default function Home() {
 			section: "Check Out",
 			body: "Key Check Out",
 			icon: "fas fa-sign-out-alt",
-			link: "/checkout",
+			link: "checkout",
 			class: "card-checkout"
 		},
 		{
@@ -38,7 +38,7 @@ export default function Home() {
 			section: "Search",
 			body: "Lookup VINs & Slots",
 			icon: "fas fa-search",
-			link: "/search",
+			link: "search",
 			class: "card-search"
 		},
 		{
@@ -46,7 +46,7 @@ export default function Home() {
 			section: "Reports",
 			body: "Generate Any & All Reports",
 			icon: "fas fa-file-alt",
-			link: "/reports",
+			link: "reports",
 			class: "card-reports"
 		},
 		{
@@ -54,7 +54,7 @@ export default function Home() {
 			section: "Dashboard",
 			body: "Dashboard Stuff",
 			icon: "fas fa-tachometer-alt",
-			link: "/dashboard",
+			link: "dashboard",
 			class: "card-dashboard"
 		}
 	];
@@ -63,7 +63,7 @@ export default function Home() {
 			<div className="grid-container grid-container-home">
 			
 				{sections.map(section => (
-					<div className={`card ${section.class}`} key={section.id} onClick={() => onClickNavigate(section.link)}>
+					<div className={`card ${section.class}`} key={section.id} onClick={() => onClickNavigate('/' + section.section.replaceAll(' ','').toLowerCase())}>
 						<h1>{section.section}</h1>
 						<p className="card-body">{section.body}</p>
 						<p className="card-icon"><FontAwesomeIcon icon={section.icon} /></p>
