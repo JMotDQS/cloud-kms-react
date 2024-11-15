da567dec0869bd6c73e90c40b334beaaff2dc967
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-11-15 15:41:12 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,49 +13,37 @@ export default function Home() {
 			id: 1,
 			section: "Labels",
 			body: "Print QR Code VIN labels",
-			icon: "fas fa-qrcode",
-			link: "labels",
-			class: "card-labels"
+			icon: "fas fa-qrcode"
 		},
 		{
 			id: 2,
 			section: "Check In",
 			body: "Key Check In",
-			icon: "fas fa-sign-in-alt",
-			link: "checkin",
-			class: "card-checkin"
+			icon: "fas fa-sign-in-alt"
 		},
 		{
 			id: 3,
 			section: "Check Out",
 			body: "Key Check Out",
-			icon: "fas fa-sign-out-alt",
-			link: "checkout",
-			class: "card-checkout"
+			icon: "fas fa-sign-out-alt"
 		},
 		{
 			id: 4,
 			section: "Search",
 			body: "Lookup VINs & Slots",
-			icon: "fas fa-search",
-			link: "search",
-			class: "card-search"
+			icon: "fas fa-search"
 		},
 		{
 			id: 5,
 			section: "Reports",
 			body: "Generate Any & All Reports",
-			icon: "fas fa-file-alt",
-			link: "reports",
-			class: "card-reports"
+			icon: "fas fa-file-alt"
 		},
 		{
 			id: 6,
 			section: "Dashboard",
 			body: "Dashboard Stuff",
-			icon: "fas fa-tachometer-alt",
-			link: "dashboard",
-			class: "card-dashboard"
+			icon: "fas fa-tachometer-alt"
 		}
 	];
 	return (
