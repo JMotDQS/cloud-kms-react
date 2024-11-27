78d1a98b0e4f921260fc33b08d239725ad09aa69
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-11-27 14:26:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,33 +2,27 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './pages/Home';
-import Search from './pages/Search';
-import Reports from './pages/Reports';
-import Dashboard from './pages/Dashboard';
 import Labels from './pages/Labels';
 import CheckIn from './pages/CheckIn';
 import CheckOut from './pages/CheckOut';
-//import Store from './Store';
-//import EmployeesComponent from './EmployeesComponent';
+import Search from './pages/Search';
+import Reports from './pages/Reports';
+import Dashboard from './pages/Dashboard';
 
 function App() {
 	return (
 		<>
 			<Navbar />
 
-			{/*<Store>
-				<EmployeesComponent />
-			</Store>*/}
-
 			<div className="main-container">
 				<Routes>
 					<Route path="/" element={<Home />} />
-					<Route path="/search" element={<Search />} />
-					<Route path="/reports" element={<Reports />} />
-					<Route path="/dashboard" element={<Dashboard />} />
 					<Route path="/labels" element={<Labels />} />
 					<Route path="/checkin" element={<CheckIn />} />
 					<Route path="/checkout" element={<CheckOut />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/reports" element={<Reports />} />
+					<Route path="/dashboard" element={<Dashboard />} />
 				</Routes>
 			</div>
 		</>
